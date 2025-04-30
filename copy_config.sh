925173e81bfe72c6522c996ca8d9091e8545e0cf
Mikkel <Mikkelhelsing1@gmail.com>
2025-04-30 21:23:48 +0200

    Wofi Styling

--- copy_config.sh
+++ copy_config.sh
@@ -4,6 +4,7 @@ rsync --recursive --force ~/.config/hypr ./assets/.config/
 rsync --recursive --force ~/.config/foot ./assets/.config/
 rsync --recursive --force ~/.config/gtk-3.0 ./assets/.config/
 rsync --recursive --force ~/.config/gtk-4.0 ./assets/.config/
+rsync --recursive --force ~/.config/wofi ./assets/.config/
 
 cp ~/.config/mimeapps.list ./assets/.config/mimeapps.list
 
