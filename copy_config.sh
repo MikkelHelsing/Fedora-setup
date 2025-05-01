a5c141e19cdce69d61e193e78de7b3b9a94f3ebc
Mikkel <Mikkelhelsing1@gmail.com>
2025-05-01 22:57:05 +0200

    Swaync and som wip styling

--- copy_config.sh
+++ copy_config.sh
@@ -5,6 +5,7 @@ rsync --recursive --force ~/.config/foot ./assets/.config/
 rsync --recursive --force ~/.config/gtk-3.0 ./assets/.config/
 rsync --recursive --force ~/.config/gtk-4.0 ./assets/.config/
 rsync --recursive --force ~/.config/wofi ./assets/.config/
+rsync --recursive --force ~/.config/swaync ./assets/.config/
 
 cp ~/.config/mimeapps.list ./assets/.config/mimeapps.list
 cp ~/.config/background/background.* ./assets/
