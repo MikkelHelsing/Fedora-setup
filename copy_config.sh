ced7408344d009ef49d558314b91dd121e9ca773
Mikkel <Mikkelhelsing1@gmail.com>
2025-05-02 22:08:47 +0200

    Added Waybar and some initial styling

--- copy_config.sh
+++ copy_config.sh
@@ -6,6 +6,7 @@ rsync --recursive --force ~/.config/gtk-3.0 ./assets/.config/
 rsync --recursive --force ~/.config/gtk-4.0 ./assets/.config/
 rsync --recursive --force ~/.config/wofi ./assets/.config/
 rsync --recursive --force ~/.config/swaync ./assets/.config/
+rsync --recursive --force ~/.config/waybar ./assets/.config/
 
 cp ~/.config/mimeapps.list ./assets/.config/mimeapps.list
 cp ~/.config/background/background.* ./assets/
