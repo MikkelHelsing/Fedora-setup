034cf4e3512a9275f550ed47785f1c1507040363
Mikkel <Mikkelhelsing1@gmail.com>
2025-05-17 16:11:12 +0200

    More background

--- copy_config.sh
+++ copy_config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rsync --recursive --force ~/.config/background ./assets/.config/
 rsync --recursive --force ~/.config/foot ./assets/.config/
 rsync --recursive --force ~/.config/gtk-3.0 ./assets/.config/
 rsync --recursive --force ~/.config/gtk-4.0 ./assets/.config/
@@ -9,7 +10,6 @@ rsync --recursive --force ~/.config/waybar ./assets/.config/
 rsync --recursive --force ~/.config/wofi ./assets/.config/
 rsync --recursive --force ~/.config/xdg-desktop-portal ./assets/.config/
 
-cp ~/.config/background/background.* ./assets/
 cp ~/.config/mimeapps.list ./assets/.config/mimeapps.list
 
 cp ~/.zshrc ./assets/
